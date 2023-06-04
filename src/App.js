3e2838d486c78dea89bc9e29752508c9f8d8eb4e
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-06-04 15:27:26 +0500

    feat: add info block

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ function App() {
           <h1 className='head'>
             ToDo List<i className='fas fa-clipboard-list headIcon'></i>
           </h1>
+          <div className='info-block'>
+            <p>changes todos order by drag and drop</p>
+            <p>press Enter or click add task button to add todo</p>
+          </div>
+
           <AddTodoForm />
+
           <TodoList></TodoList>
           {/* <button id='rmvbtn'>
             delete selected<i class='fas fa-trash-alt'></i>
